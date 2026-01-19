ef727a4c5fb49b3c87ad2879ee6787f29c284ad9
Abhinav Thakur <thakurabhinav16160@gmail.com>
2026-01-20 02:51:25 +0530

    Initial commit - MERN practice files

---
+++ basics/var.py
@@ -0,0 +1,60 @@
+name = input("enter your name:")
+age= input("enetr your age:")
+print(f"hii my name is {name} and my age is {age}")
+
+
+a=20
+b = int("22")
+print(a+b)
+
+
+Arithmetic operators
+
+a=5
+b=20
+print(a+b)
+print(b-a)
+print(a*b)
+print(b/a)
+print(b//a)
+print(5**2)
+print(32%5)
+
+
+Assignment operators
+
+compound assignment operator
+a=20
+a+=20
+a+=40
+a+=60
+print(a)
+
++= add and assign
+-= subtract and assign
+*= multiply and assign
+/= divide and assign
+//= floor divide and assign
+%= modules and assign
+**= exponentiation and assign (power)
+ 
+
+
+comparison operators----- true or false
+
+a=12
+b=12
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+
+
+
+logical oprators
+and,or,not
+print(True and bool(0))
+
+
+
+
