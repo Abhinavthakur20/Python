68ab4f3df3b3571fd50b2e9dc6e8821817951160
Abhinav Thakur <thakurabhinav16160@gmail.com>
2026-01-23 14:22:01 +0530

    loops concepts

---
+++ basics/loops.py
@@ -0,0 +1,59 @@
+
+for i in range(0,44,4):
+    print(i)
+
+
+for i in range(16,0,-1):
+    print(i)
+
+
+for i in range(20,51,1):
+    print(i)
+
+for i in range(-3,-15,-1):
+    print(i)
+
+
+for i in range(5,51,5):
+    print(i)
+
+for i in range(7,71,7):
+    print(i)
+
+
+
+
+
+
+
+
+n = int(input("enter the value of n -"))
+for i in range(n,n*10+1,n):
+    print(i)
+ 
+
+
+# loops on strings
+
+
+a= "SHERIYANS TEACHES INDUSTRY THINGS"
+for i in range (len(a)):
+    print(a[i])
+
+
+a= "SHERIYANS TEACHES INDUSTRY THINGS"
+for i in a:
+    print(i)
+    
+
+
+
+for i in range(1,21):
+    if i==15:
+        continue
+    else:
+        print(i)
+
+
+
+
