3164c2d249c710fab07eed276a7fd1d05e59fbb7
Abhinav Thakur <thakurabhinav16160@gmail.com>
2026-01-26 06:53:20 +0530

    tuple done

---
+++ basics/tuple.py
@@ -0,0 +1,16 @@
+# Tuples --IMMUTABLE -- DUPLICATES ALLOWED --ORDERED -- STORE MULTIPLEDATA TYPES
+
+# Tuple is traverse same like List traversing
+
+tup= (1,23,6,45,6,778,99)
+#methods
+index = tup.index(45)
+count = tup.count(6)
+print(index)
+print(count)
+
+
+
+
+#tuple unpacking
+a,b,c,d=(20,32,43,54)
